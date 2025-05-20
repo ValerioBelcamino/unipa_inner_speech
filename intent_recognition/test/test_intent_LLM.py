1fda756a9062f66bcb9d6f98070c17b47f227f8a
mkilina <1kilina.97@gmail.com>
2025-05-20 18:17:47 +0200

    move llm calling to a separate file

--- intent_recognition/test/test_intent_LLM.py
+++ intent_recognition/test/test_intent_LLM.py
@@ -1,132 +1,20 @@
-import pytest, os, json, random, ast
+import pytest, os, json, sys
 from langsmith import testing as t
-from dotenv import load_dotenv
-from shared_utils.customization_helpers import load_all_intent_models
-from intent_post_processing.loader import load_plugins
-from langchain.chat_models import init_chat_model
-from typing import Any, get_origin, get_args, Union
-from db_adapters import DBFactory
-from langchain.evaluation.parsing.base import JsonEqualityEvaluator
-import asyncio
+filepath = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0, filepath)
+from intent_recognition.llm import get_LLM_response
 
 os.environ["LANGSMITH_TRACING"] = "true"
 os.environ["LANGSMITH_ENDPOINT"] = "https://api.smith.langchain.com"
 os.environ["LANGSMITH_PROJECT"] = "advisor"
-
-CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
-BASE_DIR = os.path.abspath(os.path.join(CURRENT_DIR, "../../../unipa_inner_speech"))
-dotenv_path = os.path.join(BASE_DIR, ".env")
-load_dotenv(dotenv_path)
-dotconfig_path = os.path.join(BASE_DIR, ".config")
-load_dotenv(dotconfig_path)
-
 os.environ["LANGSMITH_API_KEY"] = os.getenv("LANGSMITH_API_KEY")
 
-llm_config = ast.literal_eval(os.getenv("LLM_CONFIG"))['intent_recognition']
-
-llm = init_chat_model(
-        model=llm_config['model_name'], 
-        model_provider=llm_config['model_provider'], 
-        temperature=llm_config['temperature'], 
-        api_key=os.getenv("GROQ_API_KEY")
-    )
-
-scenario = os.getenv("SCENARIO")
-print(f"\033[34mUsing {scenario}!\033[0m")
-dynamic_intent_tools_dict = load_all_intent_models(scenario)
-print(f"\033[34mLoaded {dynamic_intent_tools_dict} intent_tool(s).\033[0m")
-dynamic_intent_toolnames = [dit.__name__ for dit in dynamic_intent_tools_dict.values()]
-
-db_type = os.getenv("DB_TYPE") 
-db = DBFactory.create_adapter(db_type)
-
-llm_with_tools = llm.bind_tools(dynamic_intent_tools_dict.values())
-print(llm_with_tools.get_input_schema())
-plugins = load_plugins(scenario)
-print(f"\033[1;38;5;208mLoaded {len(plugins)} processing plugin(s).\033[0m")
-
-
-def execute_plugin_pipeline(db_adapter, action_name, intent_parameters):
-        """
-        Function to execute the loaded plugins with the appropriate parameters.
-        """
-
-        context = {
-            "db_adapter": db_adapter,  # Pass the adapter instead of the driver
-            "action_name": action_name,
-            "intent_parameters": intent_parameters  # Adding the dynamic parameters extracted after LLM computation
-        }
-        # Iterate over each loaded plugin (each wrapped function)
-        for plugin_function in plugins:
-            try:
-                # Call the plugin with the context
-                context['intent_parameters'] = plugin_function(context)
-
-            except Exception as e:
-                print(f"Error executing plugin: {e}")
-
-def get_default_value(t: Any):
-        try:
-            origin = get_origin(t)
-            
-            if origin is Union:
-                # Filter out NoneType and keep the first non-None type
-                non_none_args = [arg for arg in get_args(t) if arg is not type(None)]
-                if non_none_args:
-                    t = non_none_args[0]
-            
-            origin = get_origin(t) or t
-            value = origin()
-            
-            if callable(value):
-                return value
-            return value
-        except Exception:
-            return None
-        
-def check_undeclared_parameters(tool_class, tool_result):
-        parameter_list = [(k, get_default_value(v.annotation)) for k,v in tool_class.model_fields.items()]
-        for parameter, default_value in parameter_list:
-            if parameter not in tool_result or tool_result[parameter] is None:
-                print(f"\033[33mParameter {parameter} not found in tool result. Setting default value: {default_value}\033[0m")
-                tool_result[parameter] = default_value
-        return tool_result
-
-def get_LLM_response(user_input):
-    """
-    Function to get the LLM response for a given user input.
-    """
-    # start_time = time.time()
-    llm_response = llm_with_tools.invoke(
-                [
-                    ("system", """
-You are tasked with identifying the correct intent from a set of available tools and extracting only the parameters explicitly provided by the user.
-You must not use external knowledge, assumptions, or inference to guess or complete missing information.
-If the user input is not relevant to any of the available tools, do not respond or assign an intent.
-Only fill tool parameters when the necessary information is clearly and explicitly included in the user input.
-Do not hallucinate or fabricate any information.
-Do not fill gaps, or rephrase missing data.
-If a parameter is missing, ambiguous, or incomplete, leave it blank and do not attempt to infer or complete it.
-Follow these constraints strictly to ensure reliability and factual accuracy in tool usage."""),
-                    ("human", user_input),
-                ]
-            )
-    print(llm_response)
-    tool_calls = llm_response.tool_calls
-    print(f"\033[1;38;5;208mTool calls: '{tool_calls}'\033[0m")
-    if not tool_calls:
-        tool_name = "OutOfScope"
-        tool_result = {}
-    else:
-        tool_name = tool_calls[0]["name"]
-        tool_result = tool_calls[0]['args']
-        tool_result = check_undeclared_parameters(dynamic_intent_tools_dict[tool_name], tool_result)
-        execute_plugin_pipeline(db, tool_name, tool_result)
-    
-    return {
-        "action_name": tool_name,
-        "parameters": tool_result
-    }
+def f1_measure(expected, actual):
+    tp = sum(1 for key in expected if actual.get(key) == expected[key])
+    precision = tp / len(actual) if actual else 0
+    recall = tp / len(expected) if expected else 0
+    f1_score = (2 * precision * recall) / (precision + recall) if (precision + recall) > 0 else 0
+    return f1_score
 
 def extract_examples(filename='examples.json'):
     dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -163,15 +51,26 @@ def test_my_groq_chain(question):
     })
 
     # Call your Groq chain
-    outputs = get_LLM_response(question)
-    
-    actual_intent = outputs["action_name"]
-    actual_parameters = outputs["parameters"]
+    actual_intent, actual_parameters = get_LLM_response(question)
 
     t.log_outputs({
         "action_name": actual_intent,
         "parameters": actual_parameters
     })
+
+    t.log_feedback(
+        key="Intent Accuracy",
+        score=1 if actual_intent == expected_intent else 0
+    )
+    
+    # Calculate F1 measure for parameters
+    if expected_parameters:
+        
+        f1_score = f1_measure(expected_parameters, actual_parameters)
+        t.log_feedback(
+            key="Entities F1 Score",
+            score=f1_score
+        )
 
     # Assert the intent name
     assert actual_intent == expected_intent, f"Expected {expected_intent}, got {actual_intent}"
