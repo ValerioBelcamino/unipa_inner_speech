17aa5f720a9f0ee7e03883608e9dcc2be29eaefe
valeriobelcamino <valeriobelcamino@gmail.com>
2025-06-11 13:41:53 +0200

    launch file now has a toggle parameter for text/speech input

--- inner_speech/launch/advisor.py
+++ inner_speech/launch/advisor.py
@@ -1,16 +1,40 @@
 import launch
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch.conditions import IfCondition, UnlessCondition
 from launch_ros.actions import Node
 
 def generate_launch_description():
+    input_mode_arg = DeclareLaunchArgument(
+        'use_audio',
+        default_value='false',
+        description='Set to "true" to use speech input instead of text input.'
+    )
+
+    use_audio = LaunchConfiguration('use_audio')
+
     return LaunchDescription([
+        input_mode_arg,
+
         Node(
             package='perception_nodes',
             executable='user_input',
             name='user_input',
             output='screen',
-            prefix='gnome-terminal --title="User Input" -- '
+            prefix='gnome-terminal --title="User Input (Text)" -- ',
+            condition=UnlessCondition(use_audio)
         ),
+
+        Node(
+            package='perception_nodes',
+            executable='speech_recognition_node',
+            name='speech_input',
+            output='screen',
+            prefix='gnome-terminal --title="Speech Input" -- ',
+            condition=IfCondition(use_audio)
+        ),
+
         Node(
             package='query_generation',
             executable='query_generation',
